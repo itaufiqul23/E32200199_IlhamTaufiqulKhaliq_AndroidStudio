875a3b92b689424029f2912c0abbb367c1a24cda
ilham <taufiq@polije.ac.id>
2021-09-28 21:24:37 +0700

    Tugas Minggu 3

---
+++ app/src/main/java/com/example/e32200199_ilhamtaufiqulkhaliq/ConstraintLayout.java
@@ -0,0 +1,14 @@
+package com.example.e32200199_ilhamtaufiqulkhaliq;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConstraintLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_layout);
+    }
+}
---
+++ app/src/main/java/com/example/e32200199_ilhamtaufiqulkhaliq/LayoutDashboard.java
@@ -0,0 +1,14 @@
+package com.example.e32200199_ilhamtaufiqulkhaliq;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LayoutDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_dashboard);
+    }
+}
